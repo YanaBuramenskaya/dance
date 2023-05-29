1c9d232f8023798b061a64b893efbfd5b9f6146e
Яна <yanina.exessa@yandex.ru>
2023-05-29 22:33:43 +0300

    try to format pictures

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from "./components/Header";
 import News from "./components/News";
 import Calendar from "./components/Calendar";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import ioga from './images/ioga.jpg'
+import tansor from './images/tansor.jpg'
 
 class App extends React.Component {
     constructor(props) {
@@ -12,12 +14,20 @@ class App extends React.Component {
                 {
                     id: 1,
                     title: 'Мы открылись!!!',
-                    description: "УРА"
+                    description: "УРА",
+                    image: ioga
                 },
                 {
                     id: 2,
-                    title: 'Скидочки',
-                    description: "УРА"
+                    title: 'Мы открылись!!!',
+                    description: "Приглашаем Вас окунуться в мир шестовой акробатики, танцев на пилоне и растяжки от профессионалов своего дела.\n" +
+                        "\n" +
+                        "Если вы искали для себя достойную школу танцев, то вы попали по адресу!\n" +
+                        "\n" +
+                        "Мы предоставляем услуги обучения танцам, групповые и индивидуальные занятия. Уроки танцев помогут вам получить хорошую физическую нагрузку и отличное настроение.\n" +
+                        "\n" +
+                        "Обучение танцам в - увлекательный процесс, который познакомит вас с различными танцевальными стилями.",
+                    image: tansor
                 }
             ]
         }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,13 +2,15 @@ import React from "react";
 
 class NewsItem extends React.Component {
     news = this.props.news
+
     render() {
-        return (
-            <div className="newsItem">
-                <h3>{this.news.title}</h3>
-                <p>{this.news.description}</p>
-            </div>
-        )
+        return (<div>
+            <ul className="newsItem">
+                <li><h3>{this.news.title}</h3></li>
+                <li><p>{this.news.description}</p></li>
+                <li><img src={this.news.image} alt='blog image'/></li>
+            </ul>
+        </div>)
     }
 }
 
