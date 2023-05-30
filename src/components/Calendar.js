7d67970b1e3d29d002d8bf0e6ef55c32919d7f5f
Яна <yanina.exessa@yandex.ru>
2023-05-30 10:47:04 +0300

    add new calendar, change css

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import calendpic from './calendpic.png'
 
 class Calendar extends React.Component {
     render() {
         return (<div className="calendar-contain">
+                <img src={calendpic} alt='календарь'/>
 
         </div>
         )
